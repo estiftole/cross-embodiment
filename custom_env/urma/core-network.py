b89d9d0ab21c8cd3a841f3567c16d83d2ce712ad
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-09-15 11:38:04 +0300

    implement URMA architecture, start work on NerveNet

--- custom_env/urma/core-network.py
+++ custom_env/urma/core-network.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn as nn
 
 class CoreNetwork(nn.Module):
-    def __init__(self, joints_dim, feet_dim, general_obs_dim, hidden_dim, action_latent_dim):
+    def __init__(self, joints_dim, feet_dim, general_obs_dim, goal_dim, hidden_dim, action_latent_dim):
         super().__init__()
+        in_dim = joints_dim+feet_dim+general_obs_dim+goal_dim
         self.net = nn.Sequential(
-            nn.Linear(joints_dim+feet_dim+general_obs_dim, hidden_dim),
+            nn.Linear(in_dim, hidden_dim),
             nn.LayerNorm(hidden_dim),
             nn.ELU(),
             nn.Linear(hidden_dim, hidden_dim),
