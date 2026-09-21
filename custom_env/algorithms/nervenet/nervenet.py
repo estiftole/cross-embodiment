999b80b16dd179526ac2881be06306d716015be2
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-09-21 15:35:31 +0300

    implement critic models, add 'Actor' to model names

--- custom_env/algorithms/nervenet/nervenet.py
+++ custom_env/algorithms/nervenet/nervenet.py
@@ -5,12 +5,12 @@ from .decoder import ActionDecoder
 import torch
 import torch.nn as nn
 
-def prepare_inputs(obs_dict, graph_meta, device="cpu"):
+def prepare_inputs(obs, graph_meta, device="cpu"):
 
-    base_obs = obs_dict["base_obs"]
-    target_obs = obs_dict["target_obs"]
+    base_obs = obs["base_obs"]
+    target_obs = obs["target_obs"]
 
-    j_obs = torch.tensor(obs_dict["joint_obs"], dtype=torch.float32, device=device).unsqueeze(0)
+    j_obs = torch.tensor(obs["joint_obs"], dtype=torch.float32, device=device).unsqueeze(0)
 
     senders = graph_meta["senders"].to(device)
     receivers = graph_meta["receivers"].to(device)
@@ -25,7 +25,7 @@ def prepare_inputs(obs_dict, graph_meta, device="cpu"):
         "actuatable_nodes": actuatable_nodes
     }
 
-class NerveNet(nn.Module):
+class NerveNetActor(nn.Module):
     def __init__(self,
         j_obs_dim, target_obs_dim, base_obs_dim,
 
@@ -42,19 +42,60 @@ class NerveNet(nn.Module):
         super().__init__()
         self.target_enc = ObservationEncoder(target_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
         self.j_enc = ObservationEncoder(j_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
-        self.root_enc = ObservationEncoder(base_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
+        self.base_enc = ObservationEncoder(base_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
 
         self.gnn = GraphNN(hidden_state_dim, updater_hidden_dim, msg_hidden_dim, msg_dim, iterations)
         self.action_dec = ActionDecoder(hidden_state_dim, action_dec_hidden_dim, action_dim)
 
     def forward(self, target_obs, base_obs, j_obs, senders, receivers, actuatable_nodes):
         target_hidden = self.target_enc(target_obs)
-        j_hidden = self.target_enc(j_obs)
-        root_hidden = self.target_enc(base_obs)
+        j_hidden = self.j_enc(j_obs)
+        base_hidden = self.base_enc(base_obs)
 
-        phys_hidden = torch.cat([root_hidden, j_hidden], dim=1)
-        motor_joint_states = self.gnn(phys_hidden, target_hidden, senders, receivers)
+        phys_hidden = torch.cat([base_hidden, j_hidden], dim=1)
+        # motor_joint_states = self.gnn(phys_hidden, target_hidden, senders, receivers)
         motor_joint_states = self.gnn(phys_hidden, target_hidden, senders, receivers)[:, actuatable_nodes, :]
         actions = self.action_dec(motor_joint_states)
 
         return actions
+
+
+class NerveNetCritic(nn.Module):
+    def __init__(self,
+        j_obs_dim, target_obs_dim, base_obs_dim,
+
+        obs_enc_hidden_dim,
+        hidden_state_dim,
+
+        updater_hidden_dim, msg_hidden_dim,
+        msg_dim,
+        iterations,
+
+        value_dec_hidden_dim
+    ) -> None:
+        super().__init__()
+        self.target_enc = ObservationEncoder(target_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
+        self.j_enc = ObservationEncoder(j_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
+        self.base_enc = ObservationEncoder(base_obs_dim, obs_enc_hidden_dim, hidden_state_dim)
+
+        self.gnn = GraphNN(hidden_state_dim, updater_hidden_dim, msg_hidden_dim, msg_dim, iterations)
+
+        self.value_dec = nn.Sequential(
+            nn.Linear(hidden_state_dim, value_dec_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(value_dec_hidden_dim, 1)
+        )
+
+    def forward(self, target_obs, base_obs, j_obs, senders, receivers):
+        target_hidden = self.target_enc(target_obs)
+        j_hidden = self.j_enc(j_obs)
+        base_hidden = self.base_enc(base_obs)
+
+        phys_hidden = torch.cat([base_hidden, j_hidden], dim=1)
+        updated_phys = self.gnn(phys_hidden, target_hidden, senders, receivers)
+
+        graph_summary = updated_phys.mean(dim=1)
+
+        value = self.value_dec(graph_summary)
+
+        return value
--- custom_env/algorithms/urma/urma.py
+++ custom_env/algorithms/urma/urma.py
@@ -4,7 +4,7 @@ from .decoder import ActionDecoder
 
 import torch.nn as nn
 
-class URMA(nn.Module):
+class URMAActor(nn.Module):
     def __init__(self,
         j_obs_dim, j_desc_dim,
         ee_obs_dim, ee_desc_dim,
@@ -40,11 +40,46 @@ class URMA(nn.Module):
         j_prod = self.j_composite_enc(j_obs, j_desc)
         j_latent = self.j_attn_module(j_prod)
 
-        ee_prod = self.j_composite_enc(ee_obs, ee_desc)
-        ee_latent = self.j_attn_module(ee_prod)
+        ee_prod = self.ee_composite_enc(ee_obs, ee_desc)
+        ee_latent = self.ee_attn_module(ee_prod)
 
-        action_latent = self.core_net(j_latent, ee_latent, base_obs, target_obs)
+        action_latent = self.core_net(j_latent, ee_latent, target_obs, base_obs)
 
         action = self.action_dec(j_desc, action_latent, j_prod)
 
         return action
+
+
+class URMACritic(nn.Module):
+    def __init__(self,
+        j_obs_dim, j_desc_dim,
+        ee_obs_dim, ee_desc_dim,
+        base_obs_dim, target_obs_dim,
+
+        enc_hidden_dim,
+        j_latent_dim, ee_latent_dim,
+
+        embed_dim, attn_heads,
+
+        hidden_dim,
+    ) -> None:
+        super().__init__()
+
+        self.j_composite_enc = CompositeEncoder(j_obs_dim, j_desc_dim, enc_hidden_dim, j_latent_dim)
+        self.j_attn_module = AttentionModule(embed_dim, attn_heads)
+
+        self.ee_composite_enc = CompositeEncoder(ee_obs_dim, ee_desc_dim, enc_hidden_dim, ee_latent_dim)
+        self.ee_attn_module = AttentionModule(embed_dim, attn_heads)
+
+        self.value_net = CoreNetwork(j_latent_dim, ee_latent_dim, base_obs_dim, target_obs_dim, hidden_dim, 1)
+
+    def forward(self, target_obs, base_obs, j_obs, j_desc, ee_obs, ee_desc):
+        j_prod = self.j_composite_enc(j_obs, j_desc)
+        j_latent = self.j_attn_module(j_prod)
+
+        ee_prod = self.ee_composite_enc(ee_obs, ee_desc)
+        ee_latent = self.ee_attn_module(ee_prod)
+
+        value = self.value_net(j_latent, ee_latent, target_obs, base_obs)
+
+        return value
