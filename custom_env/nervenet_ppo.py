5cdccb4ed4419c16e59933b1ee78f43a68373d4c
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-09-26 13:07:33 +0300

    remove gradient check

--- custom_env/nervenet_ppo.py
+++ custom_env/nervenet_ppo.py
@@ -40,8 +40,6 @@ model = PPO(
 )
 
 print("Starting NerveNet PPO Training Loop...")
-model.learn(total_timesteps=100_000)
-for name, param in model.policy.actor_net.named_parameters():
-    print(f"{name}: grad={param.grad}")
+
 model.save("checkpoints/nervenet_ppo_model")
 print("Model saved successfully.")
